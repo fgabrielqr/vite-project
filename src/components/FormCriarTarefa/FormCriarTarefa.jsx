49002519554c9cb19aea936df625de9134dd64c6
fgabrielqr <fgabrielqr@gmail.com>
2024-02-26 14:19:26 -0300

    feat: :zap: Criando tarefas com estado

--- src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import { Botao, CampoTexto } from "../../components"
 import style from './FormCriarTarefa.module.css';
 
-const FormCriarTarefa = () => {
+const FormCriarTarefa = (props) => {
+    const [nomeTarefa, setNomeTarefa] = useState();
+    const onChangeNomeTarefa = (event) => {
+        setNomeTarefa(event.currentTarget.value)
+    }
+    const { setTarefas } = props;
+    const adicionarTarefa = () => {
+        event.preventDefault();
+
+        if (!nomeTarefa) {
+            return;
+        }
+        setTarefas(estadoAtual => {
+            const tarefa = {
+                id: estadoAtual.length + 1,
+                nome: nomeTarefa,
+            };
+
+            return [
+                ...estadoAtual,
+                tarefa,
+            ]
+        });
+
+        setNomeTarefa('')
+    }
+
     return (
-        <form className={style.FormCriarTarefa}>
-            <CampoTexto />
+        <form className={style.FormCriarTarefa} onSubmit={adicionarTarefa}>
+            <CampoTexto
+                value={nomeTarefa}
+                onChange={onChangeNomeTarefa}
+            />
             <Botao texto="+" />
         </form>
     )
