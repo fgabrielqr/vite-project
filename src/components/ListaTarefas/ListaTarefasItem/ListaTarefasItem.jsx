e8f9254117eed2a5cd04cc472fd68d0b7d6b42eb
fgabrielqr <fgabrielqr@gmail.com>
2024-02-28 11:06:09 -0300

    feat: :art: Interagindo com um back fake

--- src/components/ListaTarefas/ListaTarefasItem/ListaTarefasItem.jsx
+++ src/components/ListaTarefas/ListaTarefasItem/ListaTarefasItem.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import { useAppContext } from '../../../hooks';
-import { Botao, TIPO_BOTAO } from '../../Botao';
+import { Botao, CampoTexto, TIPO_BOTAO } from '../../../components';
 import style from './ListaTarefasItem.module.css';
 
 const ListaTarefasItem = (props) => {
     const { id, nome } = props;
+    const [estaEditando, setEstaEditando] = useState(false);
 
-    const { removerTarefa } = useAppContext();
+    const { editarTarefa, removerTarefa } = useAppContext();
 
     return (
         <li className={style.ListaTarefasItem}>
-            {nome}
+            {estaEditando && (
+                <CampoTexto
+                    defaultValue={nome}
+                    onChange={event => editarTarefa(id, event.currentTarget.value)}
+                    onBlur={() => setEstaEditando(false)}
+                    autoFocus
+                />
+            )}
+            {!estaEditando && (
+                <samp
+                    onDoubleClick={() => setEstaEditando(true)}>
+                    {nome}
+                </samp>
+            )}
+
             <Botao texto='-' tipo={TIPO_BOTAO.SECUNDARIO} onClick={() => removerTarefa(id)} />
         </li>
     )
